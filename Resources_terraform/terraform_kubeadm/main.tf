0040affb581d186c51e1e7b5c46dab63cd4f7657
David Mastropolo <dave.mastropolo@gmail.com>
2024-06-07 18:32:47 -0700

    main.tf update with s3 bucket, region, packer ansible AMI image and public key for the ndoes

--- Resources_terraform/terraform_kubeadm/main.tf
+++ Resources_terraform/terraform_kubeadm/main.tf
@@ -1,16 +1,18 @@
 provider "aws" {
-  region  = "eu-west-2"
+  region  = "us-east-1"
 }
 terraform {
    backend "s3" {
-    bucket = "# Add your S3 bucket here"
+    #bucket = "# Add your S3 bucket here"
+    bucket = "course3-kubeadm-terraform-state"
     key    = "default.tfstate"
-    region = "# Add the S3 bucket region"
+    region = "us-east-1"
   }
 }
 module "kubernetes" {
   source = "./kubernetes"
-  ami = "# Your configured AMI"
+  # this is the AMI id of the packer/ansible created node/master image created earlier
+  ami = "ami-0704ecf0bdb63417e"
   cluster_name = "basic-cluster"
   master_instance_type = "t3.medium"
   nodes_max_size = 1
@@ -21,5 +23,5 @@ module "kubernetes" {
   vpc_cidr_block = "10.240.0.0/16"
   worker_instance_type = "t3.medium"
   vpc_name = "kubernetes"
-  ssh_public_key = "# Add your public SSH key"
+  ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyRfQuy8VSZLn+ZX2qIlBGCzV9a6q68udgT9GoBIIYMu/NiBbMBNjt31BI+oP7bYSus1irrUKN0T7tyPEzKhLp7exdaii/7eFKI1n0RoucP9IqAx1xfXy6KUE6Nd9P6Ct2pjiLKCWzpDYx77SsoPlgn273eGXRgShdw6Qaj3vkcw6UJFbSK75SeQSK2ei17M+CUN+xvx85F8rohi+hqt5OraErgwmaOKUtS5W/5zO3w9wk+ksLBut/ltxqu8FRO79XiIhuzTOGfXFbHAN94Oni6oluvl62mDr5paJVhv++Iz4ImQcqH9aBd0uHLxa496Sd1EPeEiky62+Y1bm7WOpJOqmkMwu2GN4nNLTqwbS1UQTbtJHtObyIOM/HbT7s1P5OoaBKJbpQQ47C3F8AbbBo6AHi4RtACbGJdLdoribq32lKz6QGJ1Te8vPfIkh0wSy/5MnrmMjf98MU2cjct0MBl8UXF9e6mzdTh3fpoCAyMfoxNcAMh5CNldbQ6Px3XnE= ubuntu@ip-172-31-20-161"
 }
